6caaea998b63d5449cefdd5d87604b0c3da0bcad
Vladimir Kolesnik <vkolesni@gmail.com>
2014-07-08 21:42:50 +0300

    Update grep_awk_find.sh

--- grep_awk_find.sh
+++ grep_awk_find.sh
@@ -2,8 +2,8 @@
 #Tested on solaris (version in wiki)
 
 
-
 #fine all files (type=file), which don't have '~' in the same.
+find . -type f ! -name '~' | \
 #execute ggrep for each file" 
 #-s silent, don't print errors 
 #-h dont show file name
@@ -11,8 +11,13 @@
 #-I ignore binary files
 #-i ignore case-sensitive
 #-o only print match results, not entire line
-find . -type f ! -name '~' | xargs ggrep -shEIo 'http://(\w+).(\w+).(\w+)' | \
-egrep -vi 'w3|aol|infoaccess|stellent|webworks|apache|unicode|anyname|oracle|adobe|anyname|\
-info.com|sun.com|mycompany|userpc|microsoft|mysite|mydomain|mywebsite|autonomy|myproxy|autodesk|\
-lizardtech|iana|myhostname|mozilla|gnu|fckeditor|macromedia|proxy|filext|fastsearch|handmadesw|\
-webdav|webmaster|xmlsoap|mycomputer|ephox|outsideinsdk|yourserver|youserver|zerotouch|android' | uniq
+xargs ggrep -shEIo 'http://(\w+).(\w+).(\w+)' | \
+#second grep to filter and exclude results from first grep
+#-v invert match, eg "show all but those" (this is how we exclude stuff)
+#-i incase sens
+egrep -vi 'w3|aol|infoaccess|stellent|webworks|apache|unicode|anyname|oracle|adobe|anyname| \
+info.com|sun.com|mycompany|userpc|microsoft|mysite|mydomain|mywebsite|autonomy|myproxy|autodesk |\
+lizardtech|iana|myhostname|mozilla|gnu|fckeditor|macromedia|proxy|filext|fastsearch|handmadesw| \
+webdav|webmaster|xmlsoap|mycomputer|ephox|outsideinsdk|yourserver|youserver|zerotouch|android' \
+#uniq. Note: it will sort unique results only within the same file (not globally)
+| uniq
